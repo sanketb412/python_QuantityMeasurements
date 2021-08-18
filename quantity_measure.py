2b6a33de23fda907472fcd737f6f96dff42de4ea
sanketb412 <sanketbagde38@gmail.com>
2021-08-18 11:45:31 +0530

    [Sanket B] Add: code to convert measurement Foot into Inch

--- quantity_measure.py
+++ quantity_measure.py
@@ -7,3 +7,38 @@
 # '''
 
 print("Welcome to Quantity Measure Program.")
+class Foot_Length_Measurement():
+    """
+        A class to represent a Length Measurements.
+        ...
+        Attributes
+        ----------
+        its not taking any attributes
+        ...
+        Methods
+        -------
+        # foot_length_comparision(foot):
+        # foot_length_equality(foot, inch)
+        # foot_length_null(foot)
+        # foot_length_type(foot)
+        # foot_length_value(foot)
+    """ 
+    def foot_length_comparision(foot):
+        """
+            Description:
+                function length_comparision compare the length between foot to inch. with taking one input as an argument
+            Parameter:
+                foot as an input
+            Return:
+                printing the convertable value from foot to inch
+        """ 
+        try:
+            foot = 1
+            inch = 12*foot
+            print("1 foot = ", inch, "inch")
+        except Exception:
+            print(Exception)
+
+if __name__=='__main__':
+    foot_obj = Foot_Length_Measurement()
+    foot_obj.foot_length_comparision()
