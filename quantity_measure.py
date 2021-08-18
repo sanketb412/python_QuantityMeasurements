a9ad9d6463f7d3e51ff8ead14f44fa04ff0ad2b6
sanketb412 <sanketbagde38@gmail.com>
2021-08-18 11:35:18 +0530

    [Sanket B] Add: code of Welcomming message

---
+++ quantity_measure.py
@@ -0,0 +1,9 @@
+# ''""
+# @Author: Sanket Bagde
+# @Date: 2021-16-08
+# @Last Modified by:
+# @Last Modified time:
+# @Title :Write a Python to check length measrements.
+# '''
+
+print("Welcome to Quantity Measure Program.")
